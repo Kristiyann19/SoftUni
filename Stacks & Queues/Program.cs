719441ac171a67d49d8ae76042768144b1902c71
Kristiyan Karamilev <102369474+Kristiyann19@users.noreply.github.com>
2022-03-28 18:11:58 +0300

    Add files via upload

---
+++ Stacks & Queues/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Armory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                int[] rows = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = rows[col];
+                }
+            }
+
+
+        }
+    }
+}   
