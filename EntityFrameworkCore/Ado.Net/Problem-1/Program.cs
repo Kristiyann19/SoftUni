9caca05aff22a0446e9ffd1025759c05170defe1
Kristiyan Karamilev <102369474+Kristiyann19@users.noreply.github.com>
2022-05-22 20:50:04 +0300

    Add files via upload

---
+++ EntityFrameworkCore/Ado.Net/Problem-1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Ado.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection(@"Server=DESKTOP-VM1OT4C;Database=MinionsDB;Trusted_Connection=True;");
+
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(@"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                                  FROM Villains AS v
+                                                  JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
+                                              GROUP BY v.Id, v.Name
+                                                HAVING COUNT(mv.VillainId) > 3
+                                              ORDER BY COUNT(mv.VillainId)", conn);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            using (reader)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                while (reader.Read())
+                {
+                    string villainName = reader["Name"].ToString();
+                    string countOfMinions = reader["MinionsCount"].ToString();
+                    sb.AppendLine($"{villainName} - {countOfMinions}");
+                }
+
+                Console.WriteLine(sb.ToString().TrimEnd());
+
+            }
+        }
+    }
+}
