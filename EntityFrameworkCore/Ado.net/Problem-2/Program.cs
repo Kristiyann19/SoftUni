bb0adeddf20b83ce07ef5c540f448924a0473b80
Kristiyan Karamilev <102369474+Kristiyann19@users.noreply.github.com>
2022-05-22 20:51:28 +0300

    Add files via upload

---
+++ EntityFrameworkCore/Ado.net/Problem-2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Ado.Net_Problem_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection
+        }
+    }
+}
