124073fb240106589acd7664f3728bee9b8dc9d0
Kristiyan Karamilev <102369474+Kristiyann19@users.noreply.github.com>
2022-06-07 20:35:04 +0300

    Add files via upload

---
+++ EntityFrameworkCore/EntityFrameworkIntro/EmployeeFullInfo/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using EntityFramework.Data;
+using EntityFramework.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace EntityFramework
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext db = new SoftUniContext();
+
+            string result = GetEmployeesFullInformation(db);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Employee[] employees = context.Employees.OrderBy(e => e.AddressId).ToArray();
+
+            foreach (Employee e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
